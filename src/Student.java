1ea3016aa6816d098f445cb50164339082e05b9d
koSiukaS <a.krupauskas@gmail.com>
2016-03-30 23:23:19 +0300

    asd
    asd

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student extends Member {
+    private int courseStartYear, courseStartMonth, courseStartDay;
+    private String group;
+    private int course;
+    
+    public int getCourseStartYear(){
+        return courseStartYear;
+    }
+    
+    public void setCourseStartYear(int year){
+        courseStartYear = year;
+    }
+    
+    public int getCourseStartMonth(){
+        return courseStartMonth;
+    }
+    
+    public void setCourseStartMonth(int month){
+        courseStartMonth = month;
+    }
+    
+    public int getCourseStartDay(){
+        return courseStartDay;
+    }
+    
+    public void setDay(int day){
+        courseStartDay = day;
+    }
+    
+    public String getGroup(){
+        return group;
+    }
+    
+    public void setGroup(String group){
+        this.group = group;
+    }
+    
+    public int getCourse(){
+        return course;
+    }
+    
+    public void setCourse(String course){
+        this.course = course;
+    }
+}
