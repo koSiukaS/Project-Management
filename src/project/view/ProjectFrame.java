7832174c215aa17f353d26866315485bd402da6b
koSiukaS <a.krupauskas@gmail.com>
2016-04-20 22:08:50 +0300

    Better?
    huh?

--- src/project/view/ProjectFrame.java
+++ src/project/view/ProjectFrame.java
@@ -14,132 +14,106 @@ import project.model.UniversityProject;
 
 
 public class ProjectFrame{
-        UniversityProject Project;
-
-        public void Add(){
-        
-        final JFrame frame=new JFrame("Adding a project");
-        
-        frame.setSize(400,450);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        
-        JLabel lname=new JLabel("Project name");
-        JLabel ldescription=new JLabel("Description:");
-        JLabel lsupervisor=new JLabel("Supervisor");
-        
-        final JTextField tname=new JTextField();
+    
+    public void Add(){
+        final JFrame frameProject = new JFrame("Adding a project");
+        JLabel labelName = new JLabel("Project name");
+        JLabel labelDescription = new JLabel("Description:");
+        JLabel labelSupervisor = new JLabel("Supervisor");    
+        final JTextField textFieldName = new JTextField();       
         final JTextArea textAreaDescription = new JTextArea();
+        final JTextField textFieldSupervisor = new JTextField();
+        JButton buttonSave = new JButton("Add project");
+        
+        frameProject.setSize(400, 450);
+        frameProject.setLayout(null);
+        frameProject.setVisible(true);
+        frameProject.setLocationRelativeTo(null);
+        
         textAreaDescription.setWrapStyleWord(true);
         textAreaDescription.setLineWrap(true);
         textAreaDescription.setBorder(BorderFactory.createCompoundBorder(textAreaDescription.getBorder(),BorderFactory.createEmptyBorder(5, 5, 5, 5)));
         textAreaDescription.setFont(new Font("", Font.BOLD, 12));
         JScrollPane scrollingDescription = new JScrollPane(textAreaDescription);
+        
+        labelName.setBounds(10,10,100,30);
+        labelName.setFont(new Font("", Font.BOLD, 15));
+        textFieldName.setBounds(120,10,200,30);
+        textFieldName.setFont(new Font("", Font.BOLD, 15));
+        labelDescription.setBounds(10,50,110,30);
+        labelDescription.setFont(new Font("", Font.BOLD, 15));
         scrollingDescription.setBounds(10,90,350,180);
-        final JTextField tsupervisor=new JTextField();
-        
-        JButton save=new JButton("Add project");
-        
-        lname.setBounds(10,10,100,30);
-        lname.setFont(new Font("", Font.BOLD, 15));
-        
-        tname.setBounds(120,10,200,30);
-        tname.setFont(new Font("", Font.BOLD, 15));
-        
-        ldescription.setBounds(10,50,110,30);
-        ldescription.setFont(new Font("", Font.BOLD, 15));
-        
-        
-        lsupervisor.setBounds(10,280,100,30);
-        lsupervisor.setFont(new Font("", Font.BOLD, 15));
-        
-        tsupervisor.setBounds(120,280,200,30);
-        tsupervisor.setFont(new Font("", Font.BOLD, 15));
+        labelSupervisor.setBounds(10,280,100,30);
+        labelSupervisor.setFont(new Font("", Font.BOLD, 15));
+        textFieldSupervisor.setBounds(120,280,200,30);
+        textFieldSupervisor.setFont(new Font("", Font.BOLD, 15));
+        buttonSave.setBounds(75,320,250,80);
+        buttonSave.setFont(new Font("",Font.BOLD,25));
 
-        save.setBounds(75,320,250,80);
-        save.setFont(new Font("",Font.BOLD,25));
-
-        frame.add(lname);
-        frame.add(tname);
-        frame.add(ldescription);
-        frame.add(scrollingDescription);
-        frame.add(lsupervisor);
-        frame.add(tsupervisor);
-        frame.add(save);
+        frameProject.add(labelName);
+        frameProject.add(textFieldName);
+        frameProject.add(labelDescription);
+        frameProject.add(scrollingDescription);
+        frameProject.add(labelSupervisor);
+        frameProject.add(textFieldSupervisor);
+        frameProject.add(buttonSave);
 
 
-        save.addActionListener(new ActionListener() {
+        buttonSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-            /*newProject.setName(tname.getText());
-            newProject.setDescription(tdescription.getText());
-            newProject.setSupervisor(tsupervisor.getText());*/
-            frame.dispose();
+            frameProject.dispose();
             }
         });
     }
         public void Edit(UniversityProject project){
-        
-        final JFrame frame=new JFrame("Editing "+project.getName());
-        
-        frame.setSize(400,450);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        
-        JLabel lname=new JLabel("Project name");
-        JLabel ldescription=new JLabel("Description:");
-        JLabel lsupervisor=new JLabel("Supervisor");
-        
-        final JTextField tname=new JTextField(project.getName());
+        final JFrame frameProject = new JFrame("Editing "+project.getName());
+        JLabel labelName = new JLabel("Project name");
+        JLabel labelDescription = new JLabel("Description:");
+        JLabel labelSupervisor = new JLabel("Supervisor");    
+        final JTextField textFieldName = new JTextField(project.getName());       
         final JTextArea textAreaDescription = new JTextArea(project.getDescription());
+        final JTextField textFieldSupervisor = new JTextField(project.getSupervisor());
+        JButton buttonSave = new JButton("Save project");
+        
+        frameProject.setSize(400, 450);
+        frameProject.setLayout(null);
+        frameProject.setVisible(true);
+        frameProject.setLocationRelativeTo(null);
+        
         textAreaDescription.setWrapStyleWord(true);
         textAreaDescription.setLineWrap(true);
         textAreaDescription.setBorder(BorderFactory.createCompoundBorder(textAreaDescription.getBorder(),BorderFactory.createEmptyBorder(5, 5, 5, 5)));
         textAreaDescription.setFont(new Font("", Font.BOLD, 12));
         JScrollPane scrollingDescription = new JScrollPane(textAreaDescription);
+        
+        labelName.setBounds(10,10,100,30);
+        labelName.setFont(new Font("", Font.BOLD, 15));
+        textFieldName.setBounds(120,10,200,30);
+        textFieldName.setFont(new Font("", Font.BOLD, 15));
+        labelDescription.setBounds(10,50,110,30);
+        labelDescription.setFont(new Font("", Font.BOLD, 15));
         scrollingDescription.setBounds(10,90,350,180);
-        final JTextField tsupervisor=new JTextField(project.getSupervisor());
-        
-        JButton save=new JButton("Save project");
-        
-        lname.setBounds(10,10,100,30);
-        lname.setFont(new Font("", Font.BOLD, 15));
-        
-        tname.setBounds(120,10,200,30);
-        tname.setFont(new Font("", Font.BOLD, 15));
-        
-        ldescription.setBounds(10,50,110,30);
-        ldescription.setFont(new Font("", Font.BOLD, 15));
-        
-        scrollingDescription.setBounds(10,90,350,180);
-        
-        lsupervisor.setBounds(10,280,100,30);
-        lsupervisor.setFont(new Font("", Font.BOLD, 15));
-        
-        tsupervisor.setBounds(120,280,200,30);
-        tsupervisor.setFont(new Font("", Font.BOLD, 15));
+        labelSupervisor.setBounds(10,280,100,30);
+        labelSupervisor.setFont(new Font("", Font.BOLD, 15));
+        textFieldSupervisor.setBounds(120,280,200,30);
+        textFieldSupervisor.setFont(new Font("", Font.BOLD, 15));
+        buttonSave.setBounds(75,320,250,80);
+        buttonSave.setFont(new Font("",Font.BOLD,25));
 
-        save.setBounds(75,320,250,80);
-        save.setFont(new Font("",Font.BOLD,25));
-
-        frame.add(lname);
-        frame.add(tname);
-        frame.add(ldescription);
-        frame.add(scrollingDescription);
-        frame.add(lsupervisor);
-        frame.add(tsupervisor);
-        frame.add(save);
+        frameProject.add(labelName);
+        frameProject.add(textFieldName);
+        frameProject.add(labelDescription);
+        frameProject.add(scrollingDescription);
+        frameProject.add(labelSupervisor);
+        frameProject.add(textFieldSupervisor);
+        frameProject.add(buttonSave);
 
 
-        save.addActionListener(new ActionListener() {
+        buttonSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-            /*newProject.setName(tname.getText());
-            newProject.setDescription(tdescription.getText());
-            newProject.setSupervisor(tsupervisor.getText());*/
-            frame.dispose();
+            frameProject.dispose();
             }
         });
     }
